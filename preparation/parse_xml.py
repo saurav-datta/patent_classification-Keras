44362470b2a9f3f3c8f1e1fc738dc9164749891e
sdatta <sdatta@paypal.com>
2018-07-18 10:19:53 -0700

    Formatting changes

--- preparation/parse_xml.py
+++ preparation/parse_xml.py
@@ -1,15 +1,18 @@
 # coding: utf-8
 
 
-import xml.etree.ElementTree as ET
-import os, fnmatch
-from os.path import join
-import re
-from collections import defaultdict
 import configparser
-import shutil
-import gzip
 import datetime
+import fnmatch
+import gzip
+import os
+import re
+import shutil
+import xml.etree.ElementTree as ET
+from collections import defaultdict
+from os.path import join
+
+
 # ### Using XPATH
 
 
@@ -18,6 +21,7 @@ def get_app_ref_doc_number():
     for path in root.findall("./bibliographic-data/application-reference/document-id/doc-number"):
         app_ref_doc_num = path.text
     return app_ref_doc_num
+
 
 ######################## application_date
 def get_application_date():
@@ -36,18 +40,18 @@ def get_classification_ipcr():
         first_14 = path_text[:14]
         from_15 = path_text[14:].strip()
         first_14_list = first_14.split()
-
+        
         if (len(first_14_list) == 2):
             first_14 = first_14.replace('/', '').replace(' ', '0')
             first_14 = first_14.ljust(14, '0')
         else:
             first_14 = first_14.strip()
         #         print("path_text,first_14","|"+path_text+"|"+first_14+"|")
-
+        
         label_lookup_key_list.append(first_14)
         #         classification_text = (classification_text + "|" + (first_14 + "~" + from_15).strip()).strip('|')
         classification_text = (classification_text + "|" + first_14 + "~" + from_15).strip('|')
-
+    
     return (classification_text, label_lookup_key_list)
 
 
@@ -59,10 +63,10 @@ def get_invention_title():
         lang = path.get("lang")
         if lang == "EN":
             inv_title = path.text
-  
+    
     # TODO: Limit to top 150 words instead of first 150
     # Truncating to 150 words
-    inv_title = " ".join( inv_title.split()[:limit_word_count] )
+    inv_title = " ".join(inv_title.split()[:limit_word_count])
     
     return inv_title
 
@@ -80,10 +84,10 @@ def get_abstract_text():
                 abstract_text = abstract_text + " " + (abstract_text_tmp or '')
             abstract_text = re.sub(pattern, '', abstract_text)
             abstract_text = re.sub('\s+', ' ', abstract_text.replace('\\n', ' ')).strip()
-  
+    
     # TODO: Limit to top 150 words instead of first 150
     # Truncating to 150 words
-    abstract_text = " ".join( abstract_text.split()[:limit_word_count] )
+    abstract_text = " ".join(abstract_text.split()[:limit_word_count])
     
     return abstract_text
 
@@ -101,11 +105,11 @@ def get_description_text():
                 desc_text = desc_text + " " + (child.text or '')
             # replacing newlines and multiple spaces
             desc_text = re.sub('\s+', ' ', desc_text.replace('\\n', ' ')).strip()
-  
+    
     # TODO: Limit to top 150 words instead of first 150
     # Truncating to 150 words
-    desc_text = " ".join( desc_text.split()[:limit_word_count] )
-
+    desc_text = " ".join(desc_text.split()[:limit_word_count])
+    
     return desc_text
 
 
@@ -114,7 +118,7 @@ def get_description_text():
 def get_claim_text():
     pattern = re.compile("\([0-9,]*\)")
     # Removing words such as (6,7), (4)
-
+    
     claim_text = "NA"
     for path in root.findall("./claims"):
         lang = path.get("lang")
@@ -125,10 +129,10 @@ def get_claim_text():
                 claim_text = claim_text + " " + (claim_text_tmp or '')
             claim_text = re.sub(pattern, '', claim_text)
             claim_text = re.sub('\s+', ' ', claim_text.replace('\\n', ' ')).strip()
- 
+    
     # TODO: Limit to top 150 words instead of first 150
     # Truncating to 150 words
-    claim_text = " ".join( claim_text.split()[:limit_word_count] )
+    claim_text = " ".join(claim_text.split()[:limit_word_count])
     return claim_text
 
 
@@ -137,30 +141,31 @@ def write_files():
     with open(join(outDIR, fileNameToDocNumber), "a+") as f:
         write_str = file_path_new + "|" + app_ref_doc_num
         f.write(write_str + '\n')
-
+    
     with open(join(outDIR, fileDocNumberToClassText), "a+") as f:
         write_str = app_ref_doc_num + "|" + classification_text
         f.write(write_str + '\n')
-
+    
     with open(join(outDIR, fileDocNumberToInvTitle), "a+") as f:
         write_str = app_ref_doc_num + "|" + inv_title
         f.write(write_str + '\n')
-
+    
     with open(join(outDIR, fileDocNumberToAbsText), "a+") as f:
         write_str = app_ref_doc_num + "|" + abstract_text
         f.write(write_str + '\n')
-
+    
     with open(join(outDIR, fileDocNumberToDescText), "a+") as f:
         write_str = app_ref_doc_num + "|" + desc_text
         f.write(write_str + '\n')
-
+    
     with open(join(outDIR, fileDocNumberToClaimText), "a+") as f:
         write_str = app_ref_doc_num + "|" + claim_text
         f.write(write_str + '\n')
-
+    
     with open(join(outDIR, fileDocNumberToLabels), "a+") as f:
         write_str = app_ref_doc_num + "|" + claim_text
         f.write(write_str + '\n')
+
 
 ######################## Read label files and store them in a dictionary
 
@@ -181,36 +186,37 @@ def read_label_files():
 
 ######################## Get the labels
 
-def get_labels( l_file_path_new,  l_app_ref_doc_num):
+def get_labels(l_file_path_new, l_app_ref_doc_num):
     labels_list = []
     for key in label_lookup_key_list:
         if key not in label_dict.keys():
             # Checks for missing label keys missing in label source and writes them to error files
-            error_message=("LabelKey:{key}|File:{l_file_path_new}|AppRefDocNumber:{l_app_ref_doc_num}"
-            .format(
+            error_message = ("LabelKey:{key}|File:{l_file_path_new}|AppRefDocNumber:{l_app_ref_doc_num}"
+                .format(
                     key=key,
                     l_file_path_new=l_file_path_new,
                     l_app_ref_doc_num=l_app_ref_doc_num))
             
             with open(join(errorDIR, fileMissingLabel), "a+") as f:
                 f.write(error_message + '\n')
-
+            
             continue
         labels_list.append(label_dict[key])
     labels_string = "|".join(str(e) for e in labels_list)
     return labels_string
 
+
 ######################## Remove existing files
 def remove_files(dir_list=[], remove_zip=False):
     new_dir_list = []
     if remove_zip is True:
-        new_dir_list = [ dir_name+"_zipped" for dir_name in dir_list]
+        new_dir_list = [dir_name + "_zipped" for dir_name in dir_list]
     new_dir_list.extend(dir_list)
     for dir_name in new_dir_list:
         print("remove_files: working on {}".format(dir_name))
         if os.path.exists(dir_name) is False:
             continue
-
+        
         for filename in os.listdir(dir_name):
             filepath = os.path.join(dir_name, filename)
             try:
@@ -218,19 +224,21 @@ def remove_files(dir_list=[], remove_zip=False):
             except OSError:
                 os.remove(filepath)
 
+
 ######################## Zip output files
 def zip_output(dir_list=[]):
     for dir_name in dir_list:
-        zipDIR = dir_name+"_zipped"
-
+        zipDIR = dir_name + "_zipped"
+        
         if os.path.exists(zipDIR) is False:
             os.makedirs(zipDIR)
         for root, directories, files in os.walk(dir_name):
             for filename in files:
                 filepath = os.path.join(root, filename)
                 with open(filepath, 'rb') as f_in:
-                    with gzip.open(os.path.join(zipDIR, filename+".gz"), 'wb') as f_out:
+                    with gzip.open(os.path.join(zipDIR, filename + ".gz"), 'wb') as f_out:
                         shutil.copyfileobj(f_in, f_out)
+
 
 ######################## MAIN ########################
 
@@ -244,7 +252,6 @@ errorDIR = config['DEFAULT']['errorDIR']
 labelDIR = config['DEFAULT']['labelDIR']
 logDIR = config['DEFAULT']['logDIR']
 
-
 fileNameToDocNumber = config['DEFAULT']['fileNameToDocNumber']
 fileDocNumberToClassText = config['DEFAULT']['fileDocNumberToClassText']
 fileDocNumberToInvTitle = config['DEFAULT']['fileDocNumberToInvTitle']
@@ -254,37 +261,29 @@ fileDocNumberToClaimText = config['DEFAULT']['fileDocNumberToClaimText']
 fileDocNumberToLabels = config['DEFAULT']['fileDocNumberToLabels']
 fileMissingLabel = config['DEFAULT']['fileMissingLabel']
 
-
 labelPattern = config['DEFAULT']['labelPattern']
-limit_files_write = config.getint('DEFAULT','limit_files_write')
+limit_files_write = config.getint('DEFAULT', 'limit_files_write')
 path_string_to_replace = config['DEFAULT']['path_string_to_replace']
-limit_word_count = config.getint('DEFAULT','limit_word_count')
+limit_word_count = config.getint('DEFAULT', 'limit_word_count')
 fileList = []
 doc_number_filename = defaultdict(list)
 cnt_files = 0
 
-log_file = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+".log"
-
-
-print()
-# with open(join(logDIR, fileDocNumberToDescText), "a+") as f:
-#     write_str = app_ref_doc_num + "|" + desc_text
-#     f.write(write_str + '\n')
+log_file = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + ".log"
 
 # Read label files
 label_dict = {}
 label_dict = read_label_files()
 
 # For brevity in output file fileNameToDocNumber.txt
-pattern_path = re.compile("^"+path_string_to_replace)
+pattern_path = re.compile("^" + path_string_to_replace)
 
-#Remove existing files. The logic will also consider *_zipped directories
+# Remove existing files. The logic will also consider *_zipped directories
 
-dir_to_delete_list=[outDIR,errorDIR]
+dir_to_delete_list = [outDIR, errorDIR]
 remove_files(dir_to_delete_list, remove_zip=True)
 
-
-docNumber_date_dict={}
+docNumber_date_dict = {}
 for dName, sdName, fList in os.walk(inDIR):
     outer_break_flag = 0
     for fileName in fList:
@@ -295,59 +294,57 @@ for dName, sdName, fList in os.walk(inDIR):
             if cnt_files == limit_files_write:
                 outer_break_flag = 1
                 break
-
+            
             file_path = join(dName, fileName)
             file_path_new = re.sub(pattern_path, '', file_path)
-
+            
             tree = ET.parse(file_path)
             root = tree.getroot()
             app_ref_doc_num = get_app_ref_doc_number()
-            application_date=get_application_date()
-
+            application_date = get_application_date()
+            
             # Ignoring if patent with same application number has been seen
             if app_ref_doc_num in docNumber_date_dict.keys():
-                existing_patent_date=docNumber_date_dict[app_ref_doc_num]
+                existing_patent_date = docNumber_date_dict[app_ref_doc_num]
                 with open(join(logDIR, log_file), "a+") as f:
-                    write_str = ("doc-number {}, application_date {} has been seen with date {}".format(app_ref_doc_num,existing_patent_date,application_date))
+                    write_str = ("doc-number {}, application_date {} has been seen with date {}".format(app_ref_doc_num,
+                                                                                                        existing_patent_date,
+                                                                                                        application_date))
                     f.write(write_str + '\n')
                 continue
-
+            
             docNumber_date_dict[app_ref_doc_num] = application_date
-                
+            
             label_lookup_key_list = []
             (classification_text, label_lookup_key_list) = get_classification_ipcr()
-            labels = get_labels(file_path_new,app_ref_doc_num)
- 
+            labels = get_labels(file_path_new, app_ref_doc_num)
+            
             inv_title = get_invention_title()
             if inv_title == 'NA':
                 continue
-
+            
             abstract_text = get_abstract_text()
             if abstract_text == 'NA':
                 continue
-
+            
             desc_text = get_description_text()
             if desc_text == 'NA':
                 continue
-
+            
             claim_text = get_claim_text()
             if claim_text == 'NA':
                 continue
-
-            cnt_files = cnt_files + 1
-
-            write_files()
             
-
+            cnt_files = cnt_files + 1
+            
+            write_files()
+    
     if outer_break_flag == 1:
         break
 
 # Compress output for sharing.
-dir_to_zip_list=[outDIR,errorDIR,logDIR]
+dir_to_zip_list = [outDIR, errorDIR, logDIR]
 zip_output(dir_to_zip_list)
 
 # Not removing zipped files
 remove_files(dir_to_zip_list)
-
-
-
