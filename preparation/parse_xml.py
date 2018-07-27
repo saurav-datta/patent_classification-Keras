376b22c64669f22ac19545d41b4aba7af0999226
sdatta <sdatta@paypal.com>
2018-07-27 01:34:28 -0700

    Filtering label subclasss F only

--- preparation/parse_xml.py
+++ preparation/parse_xml.py
@@ -289,7 +289,7 @@ def zip_output(dir_list=[]):
                 with open(filepath, 'rb') as f_in:
                     with gzip.open(os.path.join(zipDIR, filename + ".gz"), 'wb') as f_out:
                         shutil.copyfileobj(f_in, f_out)
-
+    
 
 ######################## MAIN ########################
 
